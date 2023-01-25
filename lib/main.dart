90dd99302c1f1f6aed9f46cabfec0b58c34b84f4
Retian Faali <muhammadretianfaali@gmail.com>
2023-01-25 12:17:09 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,252 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/FadeAnimation.dart';
+import 'package:shopping_app/shoes.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: null,
+        title: Text(
+          "Shoes",
+          style: TextStyle(color: Colors.black, fontSize: 25),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.notifications_none,
+              color: Colors.black,
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                height: 40,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    FadeAnimation(
+                      1,
+                      AspectRatio(
+                        aspectRatio: 2.2 / 1,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "All",
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    FadeAnimation(
+                      1.1,
+                      AspectRatio(
+                        aspectRatio: 2.2 / 1,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Center(
+                            child: Text(
+                              "Sneakers",
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    FadeAnimation(
+                      1.2,
+                      AspectRatio(
+                        aspectRatio: 2.2 / 1,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Center(
+                            child: Text(
+                              "Football",
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    FadeAnimation(
+                      1.3,
+                      AspectRatio(
+                        aspectRatio: 2.2 / 1,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Center(
+                            child: Text(
+                              "Soccer",
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    FadeAnimation(
+                      1.4,
+                      AspectRatio(
+                        aspectRatio: 2.6 / 1,
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Center(
+                            child: Text(
+                              "Badminton",
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              FadeAnimation(
+                1.5,
+                makeItem(
+                    image: 'assets/images/one.jpg',
+                    tag: 'red',
+                    context: context),
+              ),
+              FadeAnimation(
+                1.6,
+                makeItem(
+                    image: 'assets/images/two.jpg',
+                    tag: 'blue',
+                    context: context),
+              ),
+              FadeAnimation(
+                1.7,
+                makeItem(
+                    image: 'assets/images/three.jpg',
+                    tag: 'white',
+                    context: context),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget makeItem({image, tag, context}) {
+  return Hero(
+    tag: tag,
+    child: GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => Shoes(image: image)));
+      },
+      child: Container(
+        height: 250,
+        width: double.infinity,
+        padding: EdgeInsets.all(20),
+        margin: EdgeInsets.only(bottom: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            image: AssetImage(image),
+            fit: BoxFit.cover,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[400],
+              blurRadius: 10,
+              offset: Offset(0, 10),
+            )
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      FadeAnimation(
+                        1,
+                        Text(
+                          "Sneakers",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      FadeAnimation(
+                        1.1,
+                        Text(
+                          "Nike",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                FadeAnimation(
+                  1.2,
+                  Container(
+                    width: 35,
+                    height: 35,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                    child: Center(
+                      child: Icon(Icons.favorite_border, size: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            FadeAnimation(
+              1.2,
+              Text(
+                "100\$",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
